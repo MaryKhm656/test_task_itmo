5718921c0d477d5d6a4c8de8e9c41ab414725a8f
MaryKhm656 <nanoman66656@gmail.com>
2025-07-26 22:05:14 +0300

    edit

--- app/logic/working_with_csv/load_csv.py
+++ app/logic/working_with_csv/load_csv.py
@@ -1,10 +1,10 @@
 import csv
 from pathlib import Path
 
-def read_csv(sub_dir: str, file_name: str):
-    path_to_csv = Path.cwd() / sub_dir / file_name
-
-    with open(path_to_csv, 'r', encoding='utf-8') as f:
+def read_csv(file_name: str):
+    csv_path = Path(file_name)
+    
+    with csv_path.open(encoding='utf-8') as f:
         reader = csv.DictReader(f)
         return list(reader)
 
---
+++ app/logic/working_with_csv/load_data.py
@@ -0,0 +1,8 @@
+from app.db import models as model
+from app.db.database import SessionLocal
+from app.logic.working_with_csv.load_csv import get_or_create, read_csv
+
+db = SessionLocal()
+rows = read_csv("Export.csv")
+for row in rows:
+    print(row["State"])
