6d5c04bc54063ba7f950a5df9b24a81523801b97
MaryKhm656 <nanoman66656@gmail.com>
2025-07-29 23:20:08 +0300

    Added functions

--- app/logic/functions.py
+++ app/logic/functions.py
@@ -1,4 +1,8 @@
-from app.db.models import User
+import math
+
+from sqlalchemy.orm import joinedload, sessionmaker
+
+from app.db.models import User, Markets, Cities, Counties, States, Review
 from app.db.database import SessionLocal
 
 def create_user(db: SessionLocal, username: str, first_name: str, last_name: str = None):
@@ -28,3 +32,70 @@ def get_user(db: SessionLocal, username: str):
         raise e
 
 
+def get_all_markets(db: SessionLocal, page: int = 1, per_page: int = 5):
+
+    try:
+        query = db.query(Markets).optioms(joinedload(Markets.reviews)).all()
+        total_pages = math.ceil(len(query) / per_page)
+        markets = query[(page - 1) * per_page: page * per_page]
+        return markets, total_pages
+    except Exception as e:
+        raise e
+
+
+def get_market_by_id(db: SessionLocal, market_id: int):
+
+    try:
+        market = db.query(Markets).filter(Markets.id == market_id).first()
+        if not market:
+            raise ValueError("Рынок с таким ID не найден")
+        return market
+    except Exception as e:
+        raise e
+
+def search_markets_by_location(db: SessionLocal, city: str = None, state: str = None, zip_code: int = None):
+    try:
+        query = db.query(Markets).join(Cities).join(Counties).join(States)
+        if city:
+            query = query.filter(Cities.name.ilike(f"%{city}%"))
+        if state:
+            query = query.filter(States.name.ilike(f"%{state}%"))
+        if zip_code:
+            query = query.filter(Markets.zip == zip_code)
+        return query.all()
+    except Exception as e:
+        raise e
+
+
+def add_review(db: SessionLocal, market_id: int, user_name: str, rating: int, text: str = ""):
+
+    try:
+        user = db.query(User).filter(username=user_name).first()
+        if not user:
+            raise ValueError("Пользователь с таким username не найден")
+
+        review = Review(
+            market_id=market_id,
+            user_id=user.id,
+            rating=rating,
+            review_text=text
+        )
+        db.add(review)
+        db.commit()
+        db.refresh()
+        return review
+    except Exception as e:
+        db.rollback()
+        raise e
+
+
+def delete_review(db: SessionLocal, review_id: int):
+    try:
+        review = db.query(Review).get(review_id)
+        if not review:
+            raise ValueError("Отзыв с таким ID Не найден!")
+        db.delete(review)
+        db.commit()
+        print("Отзыв успешно удален!")
+    except Exception as e:
+        raise e
