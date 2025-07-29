c898e332d1a910d2f282e53ec506fccc4737522a
MaryKhm656 <nanoman66656@gmail.com>
2025-07-29 21:21:54 +0300

    Edit database structure

--- app/db/init_databse.py
+++ app/db/init_databse.py
@@ -1,7 +1,6 @@
 from app.db.database import init_db
 from app.db.models import (States, Counties, Cities, Markets, MarketSeason, PaymentMethod,
-                           Products, Review, User, market_product_association, market_payment_association,
-                           user_review_association)
+                           Products, Review, User, market_product_association, market_payment_association,)
 
 if __name__ == "__main__":
     init_db()
--- app/db/models.py
+++ app/db/models.py
@@ -19,56 +19,50 @@ market_product_association = Table(
     Column("product_id", Integer, ForeignKey("products.id"), primary_key=True)
 )
 
-user_review_association = Table(
-    "user_review",
-    Base.metadata,
-    Column("user_id", Integer, ForeignKey("users.id"), primary_key=True),
-    Column("review_id", Integer, ForeignKey("reviews.id"), primary_key=True)
-)
 
 
 class States(Base):
     __tablename__ = "states"
-    
+
     id = Column(Integer, primary_key=True)
     name = Column(String(100), nullable=False)
-    
-    counties = relationship("Counties", back_populates="state")
-    
+
+    county = relationship("Counties", back_populates="state")
+
     def __str__(self):
         return f"State:\nid={self.id}\nname={self.name}"
 
 
 class Counties(Base):
     __tablename__ = "counties"
-    
+
     id = Column(Integer, primary_key=True)
     name = Column(String(100), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"))
-    
-    state = relationship("States", back_populates="counties")
-    cities = relationship("Cities", back_populates="counties")
-    
+
+    state = relationship("States", back_populates="county")
+    cities = relationship("Cities", back_populates="county")
+
     def __str__(self):
         return f"County:\nid={self.id}\nname={self.name}\nstate id={self.state_id}"
-    
+
 
 class Cities(Base):
     __tablename__ = "cities"
-    
+
     id = Column(Integer, primary_key=True)
     name = Column(String(100), nullable=False)
     county_id = Column(Integer, ForeignKey("counties.id"))
-    
+
     counties = relationship("Counties", back_populates="cities")
     markets = relationship("Markets", back_populates="cities")
-    
+
     def __str__(self):
         return f"City:\nid{self.id}\nname={self.name}\ncounty id={self.county_id}"
-    
+
 class Markets(Base):
     __tablename__ = "markets"
-    
+
     id = Column(Integer, primary_key=True)
     fmid = Column(Integer, unique=True)
     name = Column(String(100), nullable=False)
@@ -78,57 +72,61 @@ class Markets(Base):
     zip = Column(Text)
     latitude = Column(Float)
     longitude = Column(Float)
-    location = Column(Boolean)
+    has_location = Column(Boolean)
     updated_at = Column(DateTime)
-    
+
     reviews = relationship("Review", back_populates="markets")
-    cities = relationship("Cities", back_populates="markets")
-    market_season = relationship("MarketSeason", back_populates="markets")
-    payment_methods = relationship("PaymentMethod", secondary=market_payment_association, backref="markets")
-    products = relationship("Products", secondary=market_product_association, backref="markets")
-    
+    city = relationship("Cities", back_populates="markets")
+    market_seasons = relationship("MarketSeason", back_populates="markets")
+    payment_methods = relationship("PaymentMethod", secondary=market_payment_association, back_populates="markets")
+    products = relationship("Products", secondary=market_product_association, back_populates="markets")
+
     def __str__(self):
         return (f"Market:"
                 f"\nid={self.id}"
                 f"\nname={self.name}"
                 f"\ncity id={self.city_id}")
-    
-    
+
+
 class MarketSeason(Base):
     __tablename__ = "market_season"
-    
+
     id = Column(Integer, primary_key=True)
     market_id = Column(Integer, ForeignKey("markets.id"))
     season_number = Column(Integer, nullable=False)
     date_range = Column(Text)
     time_schedule = Column(Text)
-    
-    markets = relationship("Markets", back_populates="market_season")
-    
+
+    markets = relationship("Markets", back_populates="market_seasons")
+
     def __str__(self):
         return (f"Season:"
                 f"\nid={self.id}"
                 f"\nmarket_id={self.market_id}"
                 f"\nseason number={self.season_number}")
-    
-    
+
+
 class PaymentMethod(Base):
     __tablename__ = "payment_methods"
-    
+
     id = Column(Integer, primary_key=True)
     name = Column(String(100), unique=True)
-    
+
+    markets = relationship("Markets", secondary=market_payment_association, back_populates="payment_methods")
+
     def __str__(self):
         return (f"PayMethod:"
                 f"\nid={self.id}"
                 f"\nname={self.name}")
-    
+
 class Products(Base):
     __tablename__ = "products"
-    
+
     id = Column(Integer, primary_key=True)
     name = Column(String(100), unique=True)
-    
+
+    markets = relationship("Markets", secondary=market_product_association, back_populates="products")
+
     def __str__(self):
         return (f"Product:"
                 f"\nid={self.id}"
@@ -143,31 +141,31 @@ class Review(Base):
     user_id = Column(Integer, ForeignKey("users.id"))
     rating = Column(Integer)
     review_text = Column(Text)
-    created_at = Column(DateTime, default=datetime.now())
+    created_at = Column(DateTime, default=datetime.now)
 
-
+    user = relationship("User", back_populates="reviews")
     market = relationship("Markets", back_populates="reviews")
-    
+
     def __str__(self):
         return (f"Review:"
                 f"\nid={self.id}"
                 f"\nmarket_id={self.market_id}"
-                f"\nuser={self.user_last_name} {self.user_first_name}"
+                f"\nuser_id={self.user_id}"
                 f"\nrating={self.rating}"
                 f"\nreview={self.review_text}")
-    
-    
+
+
 class User(Base):
     __tablename__ = "users"
-    
+
     id = Column(Integer, primary_key=True)
     username = Column(String(100), unique=True)
     first_name = Column(String(100), nullable=False)
     last_name = Column(String(100), default=None)
-    created_at = Column(DateTime, default=datetime.now())
-    
-    review = relationship("Reviews", back_populates="users")
-    
+    created_at = Column(DateTime, default=datetime.now)
+
+    reviews = relationship("Review", back_populates="user")
+
     def __str__(self):
         return (f"User:"
                 f"\nid={self.id}"
