8c86cf96c7b39b3077493c1a30957a4d73e4e573
MaryKhm656 <nanoman66656@gmail.com>
2025-08-03 13:23:14 +0300

    added zip codes func

---
+++ app/logic/read_zip.py
@@ -0,0 +1,28 @@
+def read_zip_all():
+    i = 0
+    header = []
+    zip_codes = []
+    for line in open('zip_codes_states.csv').read().split("\n"):
+        skip_line = False
+        m = line.strip().replace('"', '').split(",")
+        i += 1
+        if i == 1:
+            for val in m:
+                header.append(val)
+        else:
+            zip_data = []
+            for idx in range(0, len(m)):
+                if m[idx] == '':
+                    skip_line = True
+                    break
+                if header[idx] == "latitude" or header[idx] == "longitude":
+                    val = float(m[idx])
+                else:
+                    val = m[idx]
+                zip_data.append(val)
+            if not skip_line:
+                zip_codes.append(zip_data)
+    return zip_codes
+
+
+print(read_zip_all())
